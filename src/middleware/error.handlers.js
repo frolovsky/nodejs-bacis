66c4158e28ea5bce83f16d20abba9da0abe3efb1
frolovsky <apf75@yandex.ru>
2020-10-24 18:49:37 +0300

    feat: add output text error

--- src/middleware/error.handlers.js
+++ src/middleware/error.handlers.js
@@ -1,11 +1,11 @@
-const { INTERNAL_SERVER_ERROR, getStatusText } = require('http-status-codes');
+const { INTERNAL_SERVER_ERROR } = require('http-status-codes');
 const { logger } = require('./logger');
 
 const errorHandler = async (err, req, res, next) => {
   if (err) {
     const status = err.status ? err.status : INTERNAL_SERVER_ERROR;
-    logger.error(JSON.stringify({ status, message: getStatusText(status) }));
-    await res.status(status).json({ message: getStatusText(status) });
+    logger.error(JSON.stringify({ status, message: err.text }));
+    await res.status(status).json(err.text);
     return;
   }
   next();
